4e5d974e17d4e464938dd68ace63603242b26c33
liuliuzo <rx78gp03@qq.com>
2021-02-15 14:47:06 +0800

    [ISSUE #219] Support to enable TLS through configuration file delete separate samples

--- rocketmq-spring-boot-samples/rocketmq-consume-demo/src/main/java/org/apache/rocketmq/samples/springboot/ExtRocketMQTemplate.java
+++ rocketmq-spring-boot-samples/rocketmq-consume-demo/src/main/java/org/apache/rocketmq/samples/springboot/ExtRocketMQTemplate.java
@@ -19,6 +19,6 @@ package org.apache.rocketmq.samples.springboot;
 import org.apache.rocketmq.spring.annotation.ExtRocketMQConsumerConfiguration;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
 
-@ExtRocketMQConsumerConfiguration(topic = "${demo.rocketmq.topic}", group = "string_consumer")
+@ExtRocketMQConsumerConfiguration(topic = "${demo.rocketmq.topic}", group = "string_consumer", tlsEnable = "${demo.ext.consumer.tlsEnable}")
 public class ExtRocketMQTemplate extends RocketMQTemplate {
 }
