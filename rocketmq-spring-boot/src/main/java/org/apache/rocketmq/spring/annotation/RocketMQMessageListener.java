16b71e4bcf745eea34e6b2fda0f661b0631188e1
liuluqi <liuluqi@kanzhun.com>
2022-03-04 11:34:15 +0800

    revert

--- rocketmq-spring-boot/src/main/java/org/apache/rocketmq/spring/annotation/RocketMQMessageListener.java
+++ rocketmq-spring-boot/src/main/java/org/apache/rocketmq/spring/annotation/RocketMQMessageListener.java
@@ -17,7 +17,11 @@
 
 package org.apache.rocketmq.spring.annotation;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.util.concurrent.LinkedBlockingQueue;
 
 @Target(ElementType.TYPE)
