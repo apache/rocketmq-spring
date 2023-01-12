837aa50daf7c4ef2185792b5c65ccc335e14b4a2
panzhi33 <panzhi33@qq.com>
2023-01-12 14:58:53 +0800

    [ISSUE #512]Go back and keep versions compatible

--- rocketmq-spring-boot/src/main/java/org/apache/rocketmq/spring/annotation/ExtRocketMQConsumerConfiguration.java
+++ rocketmq-spring-boot/src/main/java/org/apache/rocketmq/spring/annotation/ExtRocketMQConsumerConfiguration.java
@@ -32,12 +32,12 @@ import java.lang.annotation.Target;
 public @interface ExtRocketMQConsumerConfiguration {
 
     String NAME_SERVER_PLACEHOLDER = "${rocketmq.name-server:}";
-    String GROUP_PLACEHOLDER = "${rocketmq.push-consumer.group:}";
-    String TOPIC_PLACEHOLDER = "${rocketmq.push-consumer.topic:}";
+    String GROUP_PLACEHOLDER = "${rocketmq.consumer.group:}";
+    String TOPIC_PLACEHOLDER = "${rocketmq.consumer.topic:}";
     String ACCESS_CHANNEL_PLACEHOLDER = "${rocketmq.access-channel:}";
-    String ACCESS_KEY_PLACEHOLDER = "${rocketmq.push-consumer.access-key:}";
-    String SECRET_KEY_PLACEHOLDER = "${rocketmq.push-consumer.secret-key:}";
-    String TRACE_TOPIC_PLACEHOLDER = "${rocketmq.push-consumer.customized-trace-topic:}";
+    String ACCESS_KEY_PLACEHOLDER = "${rocketmq.consumer.access-key:}";
+    String SECRET_KEY_PLACEHOLDER = "${rocketmq.consumer.secret-key:}";
+    String TRACE_TOPIC_PLACEHOLDER = "${rocketmq.consumer.customized-trace-topic:}";
 
     /**
      * The component name of the Producer configuration.
