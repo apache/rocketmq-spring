d69fe60614ec3ae8996d0fa85c5daefc813ccee4
ruansheng <chrisruans@gmail.com>
2024-05-24 16:47:53 +0800

    Update DefaultRocketMQListenerContainer#getMessageType to protected

--- rocketmq-spring-boot/src/main/java/org/apache/rocketmq/spring/support/DefaultRocketMQListenerContainer.java
+++ rocketmq-spring-boot/src/main/java/org/apache/rocketmq/spring/support/DefaultRocketMQListenerContainer.java
@@ -576,7 +576,7 @@ public class DefaultRocketMQListenerContainer implements InitializingBean,
         }
     }
 
-    private Type getMessageType() {
+    protected Type getMessageType() {
         Class<?> targetClass;
         if (rocketMQListener != null) {
             targetClass = AopProxyUtils.ultimateTargetClass(rocketMQListener);
